4eb8616717ff52ee88b03b657ddd7768a6adfd8b
personal_account <fpuddu86@gmail.com>
2024-04-11 22:35:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ function getClanData() {
 
     fetch(url,{
         method: "GET",
-        mode: "no-cors",
+        mode: "cors",
         headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjNjNjdkMDY1LTZhYzUtNDY2OS04MzkwLTEyMWQ0YjVjNDNmOCIsImlhdCI6MTcxMjY1ODIyMSwic3ViIjoiZGV2ZWxvcGVyLzQ3YWNkYWM0LWEzYTAtZDJjMi0yMWI1LWE4MjgwYzI3YjJlZCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjkzLjcxLjIyNS4yMjgiXSwidHlwZSI6ImNsaWVudCJ9XX0.7UQRdwWLvt_030Ug_c6hwXvwNOngc74nDKoLQy2qgj7GQd0-tTKROkllSCKLJtomB7tReeXzSeSn9JkzSvqZJQ"
+
+            "Accept": "application/json",
+            "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjNjNjdkMDY1LTZhYzUtNDY2OS04MzkwLTEyMWQ0YjVjNDNmOCIsImlhdCI6MTcxMjY1ODIyMSwic3ViIjoiZGV2ZWxvcGVyLzQ3YWNkYWM0LWEzYTAtZDJjMi0yMWI1LWE4MjgwYzI3YjJlZCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjkzLjcxLjIyNS4yMjgiXSwidHlwZSI6ImNsaWVudCJ9XX0.7UQRdwWLvt_030Ug_c6hwXvwNOngc74nDKoLQy2qgj7GQd0-tTKROkllSCKLJtomB7tReeXzSeSn9JkzSvqZJQ"
           }
 
     })
