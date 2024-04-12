753e1f0af4aae5cd0e4aa878ee726693a4f525f8
personal_account <fpuddu86@gmail.com>
2024-04-12 23:43:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@
 
 async function getClanData() {
     const myHeaders = new Headers();
-    myHeaders.append("Host", "https://projectaccount-lab.github.io/cocAdmin/");
+    // myHeaders.append("Host", "https://projectaccount-lab.github.io/cocAdmin/");
 
 myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjQwNmU5Yjg4LTJjZjEtNGFlMy1iZGMyLWU4ZTI3NDUyMjVhMSIsImlhdCI6MTcxMjg2OTE5Niwic3ViIjoiZGV2ZWxvcGVyLzQ3YWNkYWM0LWEzYTAtZDJjMi0yMWI1LWE4MjgwYzI3YjJlZCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjkzLjcxLjIyNS4yMjgiXSwidHlwZSI6ImNsaWVudCJ9XX0.3uSeTxn2LROUD7-4Ilq0sYzM_A_qpusI1mXAxcKyvf3gSdJYTqhNUvQLawiRehpdtcTReAomy7AoC2MgTtiX-g");
 myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-myHeaders.append("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Methods X-Requested-With, content-type, Authorization");
-myHeaders.append("Access-Control-Allow-Credentials", "true");
-myHeaders.append("X-Requested-With", "XMLHttpRequest");
+// myHeaders.append("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+// myHeaders.append("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Methods X-Requested-With, content-type, Authorization");
+// myHeaders.append("Access-Control-Allow-Credentials", "true");
+// myHeaders.append("X-Requested-With", "XMLHttpRequest");
 
 const requestOptions = {
   method: "GET",
